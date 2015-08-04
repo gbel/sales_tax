6f3aa4477b546283e1e9ba2292f5b825da20f2eb
Gabe De Oliveira <gabriel@olivox.ca>
2015-08-03 23:09:01 -0400

    Refactored tests with setUp

--- src/receipt_tests.py
+++ src/receipt_tests.py
@@ -3,12 +3,28 @@ import unittest
 from receipt import Receipt
 
 class TestReceipt(unittest.TestCase):
+
+    def setUp(self):
+        self.input1_a = Receipt(1, 'book', '12.49')
+        self.input1_b = Receipt(1, 'music CD', '14.99')
+        self.input1_c = Receipt(1, 'chocolate bar', '0.85')
+
+        self.input2_a = Receipt(1, 'imported box of chocolates', '10.00')
+        self.input2_b = Receipt(1, 'imported bottle of perfume', '47.50')
+
+        self.input3_a = Receipt(1, 'imported bottle of perfume', '27.99')
+        self.input3_b = Receipt(1, 'bottle of perfume', '18.99')
+        self.input3_c = Receipt(1, 'packet of headache pills', '9.75')
+        self.input3_d = Receipt(1, 'box of imported chocolates', '11.25')
+
     def test_tax_exempt_products(self):
-        self.assertTrue(Receipt(1, 'book', 10).product_tax_exempt())
-        self.assertFalse(Receipt(1, 'Music CD', 10).product_tax_exempt())
-        self.assertTrue(Receipt(1, 'chocolate bar', 10).product_tax_exempt())
-        self.assertTrue(Receipt(1, 'imported box of chocolates', 10).product_tax_exempt())
+        self.assertTrue(self.input1_a.product_tax_exempt())
+        self.assertFalse(self.input1_b.product_tax_exempt())
+        self.assertTrue(self.input1_c.product_tax_exempt())
+        self.assertTrue(self.input2_a.product_tax_exempt())
 
     def test_imported_products(self):
-        self.assertTrue(Receipt(1, 'imported box of chocolates', 10).product_imported())
-        self.assertFalse(Receipt(1, 'Music CD', 10).product_imported())
+        self.assertTrue(self.input2_a.product_imported())
+        self.assertFalse(self.input3_b.product_imported())
+
+
